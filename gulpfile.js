78765ee60ef5188f03da31a1a00eeda9a4cec3bd
Cinti <cinti@example.com>
2021-09-09 11:30:37 +0200

    Init

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const { src, dest, watch } = require('gulp');
+	
+function streamHtml(){
+	return src('src/*.html')
+          .pipe(dest('public'));
+}
+
+function streamJS(){
+	return src('src/js/*.js')
+          .pipe(dest('public'));
+}
+
+function streamJquery(){
+        return src('node_modules/jquery/dist/jquery.min.js')
+        .pipe(dest('public/lib'));
+}
+
+exports.jquery = streamJquery;
+exports.html = streamHtml;
+exports.default = function(){
+        watch('src/*.html', streamHtml)
+        watch('src/js/*.js', streamJS)
+}
